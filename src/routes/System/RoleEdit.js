46bbd38c45ac33bded0449c7108bf3146888762f
swustdyd <1562044678@qq.com>
2018-08-20 21:20:58 +0800

    添加了权限选择的初始化功能

--- src/routes/System/RoleEdit.js
+++ src/routes/System/RoleEdit.js
@@ -1,181 +1,225 @@
-import React from 'react'
+import React from 'react';
 import { Form, Button, Input, Icon, Tree } from 'antd';
 import { connect } from 'dva';
-import { menuData } from '../../common/menu'
+import { menuData } from '../../common/menu';
 
-import styles from './RoleEdit.less'
+import styles from './RoleEdit.less';
 
 const FormItem = Form.Item;
-const {TreeNode} = Tree;
+const { TreeNode } = Tree;
 
 const menusToTree = (menus, basePath = '') => {
-    const tree = [];
-    menus.forEach((menu) => {
-        const node = {
-            title: menu.name,
-            key: `${basePath ? `${basePath}/` : ''}${menu.path}`,
-            icon: menu.icon,
-        }
-        if(menu.children && menu.children.length > 0){
-            node.children = menusToTree(menu.children, menu.path);
-        }
-        tree.push(node);
-    })
-    return tree;
-}
+  const tree = [];
+  menus.forEach(menu => {
+    const node = {
+      title: menu.name,
+      key: `${basePath ? `${basePath}/` : ''}${menu.path}`,
+      icon: menu.icon,
+    };
+    if (menu.children && menu.children.length > 0) {
+      node.children = menusToTree(menu.children, menu.path);
+    }
+    tree.push(node);
+  });
+  return tree;
+};
 
-const treeToMenus = (tree) => {
-    const menus = [];
-    tree.forEach((node) => {
-        const paths = node.key.spilt('/');
-        const menu = {
-            name: node.title,
-            path: paths[paths.length - 1],
-            icon: node.icon,
-        }
-        if(node.children && node.children.length > 0){
-            menu.children = treeToMenus(node.children);
-        }
-        menus.push(menu);
-    })
-    return menus;
-}
+const treeData = menusToTree(menuData);
 
-@connect(({role, loading}) => ({
-    role,
-    submiting: loading.effects['tag/saveOrUpdateRole'],
+const selectedTreeToMenus = tree => {
+  const menus = [];
+  tree.forEach(node => {
+    if (node.selected) {
+      const paths = node.key.split('/');
+      const menu = {
+        name: node.title,
+        path: paths[paths.length - 1],
+        icon: node.icon,
+      };
+      if (node.children && node.children.length > 0) {
+        menu.children = selectedTreeToMenus(node.children);
+      }
+      menus.push(menu);
+    }
+  });
+  return menus;
+};
+
+const getCheckedKeyFromMenus = (menus = [], basePath) => {
+  const checkedKeys = [];
+  menus.forEach(menu => {
+    if (menu.children && menu.children.length > 0) {
+      checkedKeys.push(...getCheckedKeyFromMenus(menu.children, menu.path));
+    } else {
+      checkedKeys.push(`${basePath ? `${basePath}/` : ''}${menu.path}`);
+    }
+  });
+  return checkedKeys;
+};
+
+@connect(({ role, loading }) => ({
+  role,
+  submiting: loading.effects['tag/saveOrUpdateRole'],
 }))
 @Form.create()
-export default class Edit extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            expandedKeys: [],
-            autoExpandParent: true,
-            checkedKeys: [],
-            selectedKeys: [],
-        }
+export default class Edit extends React.Component {
+  constructor(props) {
+    super(props);
+    let checkedKeys = [];
+    if (props.defaultRole) {
+      checkedKeys = getCheckedKeyFromMenus(props.defaultRole.menus);
     }
+    this.state = {
+      expandedKeys: [],
+      autoExpandParent: true,
+      checkedKeys,
+      selectedKeys: [],
+    };
+  }
 
-    onExpand = (expandedKeys) => {
-        console.log('onExpand', expandedKeys);
-        // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-        // or, you can remove all expanded children keys.
-        this.setState({
-            expandedKeys,
-            autoExpandParent: false,
-        });
-    }
+  onExpand = expandedKeys => {
+    this.setState({
+      expandedKeys,
+      autoExpandParent: false,
+    });
+  };
 
-    onCheck = (checkedKeys) => {
-        console.log('onCheck', checkedKeys);
-        this.setState({ checkedKeys });
-    }
+  onCheck = (checkedKeys, { halfCheckedKeys }) => {
+    this.setState({
+      checkedKeys,
+      halfCheckedKeys,
+    });
+  };
 
-    onSelect = (selectedKeys, info) => {
-        console.log('onSelect', info);
-        this.setState({ selectedKeys });
-        info.node.onExpand(info);
-    }
+  onSelect = (selectedKeys, info) => {
+    this.setState({ selectedKeys });
+    info.node.onExpand(info);
+  };
 
-    handleSubmit(e){
-        e.preventDefault();
-        const {form, dispatch} = this.props;
-        form.validateFieldsAndScroll((err, values) => {
-            if(!err){
-                const role = values;
-                dispatch({
-                    type: 'role/saveOrUpdateRole',
-                    payload: {role},
-                })
-            }
-        })
-    }
-
-    renderTreeNodes(data){
-        return data.map((item) => {
-          if (item.children) {
-            return (
-                <TreeNode title={item.title} key={item.key} dataRef={item}>
-                    {this.renderTreeNodes(item.children)}
-                </TreeNode>
-            );
-          }
-          return <TreeNode {...item} />;
-        });
-    }
-
-    render(){
-        const {defaultRole = {}, submiting, form:{getFieldDecorator}} = this.props;
-        const {expandedKeys, autoExpandParent, checkedKeys, selectedKeys} = this.state;
-        const formItemLayout = {
-            labelCol: {
-                xs: { span: 4 },
-                sm: { span: 4 },
-            },
-            wrapperCol: {
-                xs: { span: 20 },
-                sm: { span: 20 },
-                md: { span: 20 },
-            },
+  setSelectTree = (tree = [], selectedKey) => {
+    return tree.map(node => {
+      if (node.key === selectedKey) {
+        return {
+          ...node,
+          selected: true,
         };
-        return(
-            <Form onSubmit={(e) => this.handleSubmit(e)}>
-                <FormItem {...formItemLayout} label="角色名称">
-                    {getFieldDecorator('name', {
-                        rules: [
-                            {
-                                required: true,
-                                message: '角色名称不能为空',
-                            },  
-                        ],
-                        initialValue: defaultRole.name,
-                    })(
-                        <Input onChange={this.handleNameChange} placeholder="请填入角色名称..." />
-                    )}
-                </FormItem>
-                <FormItem {...formItemLayout} label="类型名称">
-                    {getFieldDecorator('typeName', {
-                        rules: [
-                            {
-                                required: true,
-                                message: '类型名称不能为空',
-                            },  
-                            {
-                                pattern: /^[a-zA-Z]{3,20}$/,
-                                message: '类型名称必须是3-20位字母',
-                            }, 
-                        ],
-                        initialValue: defaultRole.typeName,
-                    })(
-                        <Input placeholder="请填入类型名称..." />
-                    )}
-                </FormItem>
-                <FormItem {...formItemLayout} label="权限">
-                    <Tree
-                        checkable
-                        onExpand={this.onExpand}
-                        expandedKeys={expandedKeys}
-                        autoExpandParent={autoExpandParent}
-                        onCheck={this.onCheck}
-                        checkedKeys={checkedKeys}
-                        onSelect={this.onSelect}
-                        selectedKeys={selectedKeys}
-                    >
-                        {this.renderTreeNodes(menusToTree(menuData))}
-                    </Tree>
-                </FormItem>
-                <FormItem className={styles.submitContainer}>
-                    <Button type="primary" htmlType="submit">
-                        {submiting ? (
-                            <span>
-                                <Icon type="loading" />&nbsp;提交中...
-                            </span>
-                        ) : '提交'}
-                    </Button>
-                </FormItem>
-            </Form>
-        )
-    }
+      } else if (node.children && node.children.length > 0) {
+        return {
+          ...node,
+          children: this.setSelectTree(node.children, selectedKey),
+        };
+      } else {
+        return node;
+      }
+    });
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { form, dispatch } = this.props;
+    const { checkedKeys, halfCheckedKeys } = this.state;
+    form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        const role = values;
+        let newTreeData = treeData;
+        checkedKeys.concat(halfCheckedKeys).forEach(key => {
+          newTreeData = this.setSelectTree(newTreeData, key);
+        });
+        role.menus = selectedTreeToMenus(newTreeData);
+        dispatch({
+          type: 'role/saveOrUpdateRole',
+          payload: { role },
+        });
+      }
+    });
+  }
+
+  renderTreeNodes(data) {
+    return data.map(item => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.title} key={item.key} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode {...item} />;
+    });
+  }
+
+  render() {
+    const {
+      defaultRole = {},
+      submiting,
+      form: { getFieldDecorator },
+    } = this.props;
+    const { expandedKeys, autoExpandParent, checkedKeys, selectedKeys } = this.state;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 4 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 20 },
+        sm: { span: 20 },
+        md: { span: 20 },
+      },
+    };
+    return (
+      <Form onSubmit={e => this.handleSubmit(e)}>
+        <FormItem {...formItemLayout} label="角色名称">
+          {getFieldDecorator('name', {
+            rules: [
+              {
+                required: true,
+                message: '角色名称不能为空',
+              },
+            ],
+            initialValue: defaultRole.name,
+          })(<Input onChange={this.handleNameChange} placeholder="请填入角色名称..." />)}
+        </FormItem>
+        <FormItem {...formItemLayout} label="类型名称">
+          {getFieldDecorator('typeName', {
+            rules: [
+              {
+                required: true,
+                message: '类型名称不能为空',
+              },
+              {
+                pattern: /^[a-zA-Z]{3,20}$/,
+                message: '类型名称必须是3-20位字母',
+              },
+            ],
+            initialValue: defaultRole.typeName,
+          })(<Input placeholder="请填入类型名称..." />)}
+        </FormItem>
+        <FormItem {...formItemLayout} label="权限">
+          <Tree
+            checkable
+            onExpand={this.onExpand}
+            expandedKeys={expandedKeys}
+            autoExpandParent={autoExpandParent}
+            onCheck={this.onCheck}
+            checkedKeys={checkedKeys}
+            onSelect={this.onSelect}
+            selectedKeys={selectedKeys}
+          >
+            {this.renderTreeNodes(treeData)}
+          </Tree>
+        </FormItem>
+        <FormItem className={styles.submitContainer}>
+          <Button type="primary" htmlType="submit">
+            {submiting ? (
+              <span>
+                <Icon type="loading" />
+                &nbsp;提交中...
+              </span>
+            ) : (
+              '提交'
+            )}
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
 }
--- src/routes/System/RoleList.js
+++ src/routes/System/RoleList.js
@@ -1,129 +1,171 @@
-import React from 'react'
+import React from 'react';
 import { Table, Popconfirm, Button, Modal, Icon } from 'antd';
 import { connect } from 'dva';
-import RoleEdit from './RoleEdit'
+import RoleEdit from './RoleEdit';
 
-import styles from './RoleList.less'
+import styles from './RoleList.less';
 
-@connect(({role, loading}) => ({
-    role,
-    searching: loading.effects['role/searchRoles'],
+@connect(({ role, loading }) => ({
+  role,
+  searching: loading.effects['role/searchRoles'],
 }))
-export default class List extends React.Component{
+export default class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalSetting: {},
+      modalContent: {},
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            modalSetting: {},
-            modalContent: {},
-        }
-    }
+  componentDidMount() {
+    this.handleSearchRole();
+  }
 
-    componentDidMount(){
-        this.handleSearchRole();
-    }
+  handleSearchRole() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'role/searchRoles',
+    });
+  }
 
-    handleSearchRole(){
-        const {dispatch} = this.props;
-        dispatch({
-            type: 'role/searchRoles',
-        })
-    }
+  handleNewRole() {
+    const { dispatch } = this.props;
+    this.setState({
+      modalSetting: {
+        title: '新增角色',
+      },
+      modalContent: <RoleEdit />,
+    });
+    dispatch({
+      type: 'role/showModal',
+    });
+  }
 
-    handleNewRole(){
-        const {dispatch} = this.props;
-        this.setState({
-            modalSetting: {
-                title: '新增角色',
-            },
-            modalContent: <RoleEdit />,
-        })
-        dispatch({
-            type: 'role/showModal',
-        })
-    }
+  handleDeleteRole(role) {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'role/deleteRole',
+      payload: { role },
+    });
+  }
 
-    handleDeleteRole(role){
-        const {dispatch} = this.props;
-        dispatch({
-            type: 'role/deleteRole',
-            payload: {role},
-        })
-    }
-
-    render(){
-        const {role:{list, showModal}, searching, dispatch} = this.props;        
-        const {modalSetting, modalContent} = this.state;
-        const columns = [
-            {
-                title: '名称',
-                dataIndex: 'name',
-                render: (name, record) => (
-                    <a 
-                        onClick={() => {
-                            this.setState({
-                                modalSetting: {
-                                    title: `编辑角色：“${name}”`,
-                                },
-                                modalContent: <RoleEdit defaultRole={record} />,
-                            })
-                            dispatch({
-                                type: 'role/showModal',
-                            })
-                        }}
-                    >
-                        {name}
-                    </a>
-                ),
-            },
-            {
-                title: '创建者',
-                dataIndex: 'creater',
-                render: name => name || 'unknown',
-            },
-            {
-                title: '编辑者',
-                dataIndex: 'editer',
-                render: name => name || 'unknown',
-            },
-            {
-                title: '最后更新时间',
-                dataIndex: 'updateAt',
-            },
-            {
-                dataIndex: 'id',
-                render: (id, record) => (
-                    <Popconfirm title={`是否删除“${record.name}”`} okText="是" cancelText="否" onConfirm={() => this.handleDeleteRole(record)}>
-                        <Icon className={styles.deleteIcon} type="delete" />
-                    </Popconfirm>
-                ),
-            },
-        ];
-        return(
-            <div className={styles.container}>
-                <Button type="primary" onClick={() => this.handleNewRole()}>新增角色</Button>
-                <Table
-                    className={styles.table}
-                    rowKey="id"
-                    columns={columns}  
-                    loading={searching} 
-                    dataSource={list} 
-                />
-                <Modal 
-                    destroyOnClose
-                    visible={showModal}
-                    maskClosable={false}
-                    footer={null}
-                    {...modalSetting}
-                    onCancel={() => {
-                        dispatch({
-                            type: 'role/hideModal',
-                        })
+  render() {
+    const {
+      role: { list, showModal },
+      searching,
+      dispatch,
+    } = this.props;
+    const { modalSetting, modalContent } = this.state;
+    const columns = [
+      {
+        title: '名称',
+        dataIndex: 'name',
+        render: (name, record) => (
+          <a
+            onClick={() => {
+              this.setState({
+                modalSetting: {
+                  title: `编辑角色：“${name}”`,
+                },
+                modalContent: (
+                  <RoleEdit
+                    defaultRole={{
+                      ...record,
+                      menus: [
+                        {
+                          name: '文章管理',
+                          path: 'article',
+                          icon: 'file-text',
+                          children: [
+                            { name: '文章列表', path: 'list' },
+                            { name: '标签设置', path: 'tag' },
+                          ],
+                        },
+                        {
+                          name: '系统设置',
+                          path: 'system',
+                          icon: 'setting',
+                          children: [
+                            { name: '角色设置', path: 'role' },
+                            { name: '用户列表', path: 'user-list' },
+                          ],
+                        },
+                        {
+                          name: '我的',
+                          path: 'user',
+                          icon: 'user',
+                          children: [{ name: '个人中心', path: 'detail' }],
+                        },
+                      ],
                     }}
-                >
-                    {modalContent}
-                </Modal>
-            </div>
-        )
-    }
+                  />
+                ),
+              });
+              dispatch({
+                type: 'role/showModal',
+              });
+            }}
+          >
+            {name}
+          </a>
+        ),
+      },
+      {
+        title: '创建者',
+        dataIndex: 'creater',
+        render: name => name || 'unknown',
+      },
+      {
+        title: '编辑者',
+        dataIndex: 'editer',
+        render: name => name || 'unknown',
+      },
+      {
+        title: '最后更新时间',
+        dataIndex: 'updateAt',
+      },
+      {
+        dataIndex: 'id',
+        render: (id, record) => (
+          <Popconfirm
+            title={`是否删除“${record.name}”`}
+            okText="是"
+            cancelText="否"
+            onConfirm={() => this.handleDeleteRole(record)}
+          >
+            <Icon className={styles.deleteIcon} type="delete" />
+          </Popconfirm>
+        ),
+      },
+    ];
+    return (
+      <div className={styles.container}>
+        <Button type="primary" onClick={() => this.handleNewRole()}>
+          新增角色
+        </Button>
+        <Table
+          className={styles.table}
+          rowKey="id"
+          columns={columns}
+          loading={searching}
+          dataSource={list}
+        />
+        <Modal
+          destroyOnClose
+          visible={showModal}
+          maskClosable={false}
+          footer={null}
+          {...modalSetting}
+          onCancel={() => {
+            dispatch({
+              type: 'role/hideModal',
+            });
+          }}
+        >
+          {modalContent}
+        </Modal>
+      </div>
+    );
+  }
 }
