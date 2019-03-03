e0c98aee4a8e755f3cabe046948d951fb9b6f6fa
dyd <1562044678@qq.com>
2019-03-03 14:05:22 +0800

    自动打包命令支持输入提交信息

--- deploy/staging.js
+++ deploy/staging.js
@@ -2,6 +2,8 @@ const shelljs = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 
+const commitMessage = process.argv[2] ? `auto deploy(${process.argv[2]})` : 'auto deploy';
+
 shelljs.exec('cross-env ESLINT=none NODE_ENV=staging roadhog build', (code) => {
   if (code !== 0) {
     throw new Error('build failed');
@@ -21,7 +23,7 @@ shelljs.exec('cross-env ESLINT=none NODE_ENV=staging roadhog build', (code) => {
   }
   shelljs.cp('-rf', path.resolve(__dirname, '../dist'), distPath);
   shelljs.exec('git add .');
-  shelljs.exec('git commit -m "auto deploy"');
+  shelljs.exec(`git commit -m "${commitMessage}"`);
   shelljs.exec('git push origin staging');
   process.exit(0);
 });
