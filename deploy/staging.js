0ed9000c932c9a17778183027fc46441fcd6bd0d
dyd <1562044678@qq.com>
2019-03-03 12:10:20 +0800

    添加打包脚本

---
+++ deploy/staging.js
@@ -0,0 +1,27 @@
+const shelljs = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+
+shelljs.exec('cross-env ESLINT=none roadhog build', (code) => {
+  if (code !== 0) {
+    throw new Error('build failed');
+  }
+  const deployPath = path.resolve(__dirname, '../../blog-manage-client-deploy');
+  if (!fs.existsSync(deployPath)) {
+    throw new Error(`depoly path '${deployPath}' not exist`);
+  }
+  
+  shelljs.cd(deployPath);
+  shelljs.exec('git checkout staging');
+  shelljs.exec('git pull origin staging');
+  
+  const distPath = path.resolve(deployPath, 'dist');
+  if (!fs.existsSync(distPath)) {
+    shelljs.rm('-rf', distPath);
+  }
+  shelljs.cp('-rf', path.resolve(__dirname, '../dist'), distPath);
+  shelljs.exec('git add .');
+  shelljs.exec('git commit -m "auto deploy"');
+  shelljs.exec('git push origin staging');
+  process.exit(0);
+});
