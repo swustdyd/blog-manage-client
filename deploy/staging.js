f0e3743245ace6320e5f61b1695d20ae3f2e105b
dyd <1562044678@qq.com>
2019-03-03 15:29:58 +0800

    打包和发布命令分开

--- deploy/staging.js
+++ deploy/staging.js
@@ -15,15 +15,24 @@ shelljs.exec('cross-env ESLINT=none NODE_ENV=staging roadhog build', (code) => {
   
   shelljs.cd(deployPath);
   shelljs.exec('git checkout staging');
-  shelljs.exec('git pull origin staging');
-  
-  const distPath = path.resolve(deployPath, 'dist');
-  if (!fs.existsSync(distPath)) {
-    shelljs.rm('-rf', distPath);
-  }
-  shelljs.cp('-rf', path.resolve(__dirname, '../dist'), distPath);
-  shelljs.exec('git add .');
-  shelljs.exec(`git commit -m "${commitMessage}"`);
-  shelljs.exec('git push origin staging');
-  process.exit(0);
+  shelljs.exec('git pull origin staging', () => {
+    const distPath = path.resolve(deployPath, 'dist');
+    // eslint-disable-next-line
+    console.log(`distPath: ${distPath}/`);
+    if (fs.existsSync(distPath)) {
+      const rmCommand = `rm -rf ${distPath}`;
+      // eslint-disable-next-line
+      console.log(rmCommand);
+      shelljs.exec(rmCommand);
+    }
+    shelljs.mkdir(distPath);
+    const cpCommand = `cp -rf ${path.resolve(__dirname, '../dist')}/ ${distPath}/`
+    // eslint-disable-next-line
+    console.log(cpCommand);
+    shelljs.exec(cpCommand);
+    shelljs.exec('git add .');
+    shelljs.exec(`git commit -m "${commitMessage}"`);
+    shelljs.exec('git push origin staging');
+    process.exit(0);
+  });
 });
