889201b5e6ef88b8d0a04709b3773c342d24ccf2
dyd <1562044678@qq.com>
2019-03-03 12:54:24 +0800

    输出当前node环境

--- config/index.js
+++ config/index.js
@@ -3,6 +3,9 @@ import stagingConfig from '../../config/index-staging';
 
 let config = baseConfig;
 
+// eslint-disable-next-line
+console.log(`NODE_ENV is '${process.env.NODE_ENV}'`)
+
 if (process.env.NODE_ENV === 'staging') {
   config = stagingConfig;
 }
